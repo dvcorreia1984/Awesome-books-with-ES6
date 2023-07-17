47d000912696757459042756a7acf5886b07783c
dvcorreia1984 <victor@onsleer.co.za>
2023-07-17 14:17:40 +0200

    Added booksArr.js file

---
+++ modules/booksArr.js
@@ -0,0 +1,14 @@
+const booksArr = [
+  {
+    Id: 1,
+    title: 'Lorem ipsum',
+    author: 'Testeroo Testyy',
+  },
+  {
+    Id: 2,
+    title: 'Second Books',
+    author: 'Testeroo Testyy',
+  },
+];
+
+export default { booksArr };
